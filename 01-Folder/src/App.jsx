4913802b8e22baeb85b4eb14d33761867e1fc84a
sachin gupta <sachingupta58111@gmail.com>
2026-09-19 20:05:46 +0530

    01-Folder

---
+++ 01-Folder/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+const App = () => {
+  return (
+    <div id="root">
+    <div id="dad">
+      <h1>hello</h1>
+      <h2>hello</h2>
+    
+      
+    </div>
+    <div id="chacha"></div>
+    </div>
+  )
+  
+}
+
+export default App
---
+++ script.js
@@ -0,0 +1,4 @@
+const  h1=document.createElement('h1')
+h1.innerhtml='hello from js'
+document.body.appendChild(h1)
+console.log(h1)
